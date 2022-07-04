083d4ce3e462f42b10ccf5aea80fd9cd5dd64117
Ben-Boyte <zissette1@gmail.com>
2022-07-04 10:28:01 -0500

    added tables and console log for host

--- index.js
+++ index.js
@@ -1,3 +1,13 @@
 const inquirer = require('inquirer');
 const cTable = require('console.table');
 const mysql = require('mysql2');
+const db = mysql.createConnection(
+    {
+        host: 'localhost',
+        user: 'root',
+        password: 'password',
+        database: 'staff_management'
+    },
+    console.log('Connected to the staff_management database.')
+);
+
